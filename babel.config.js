4788887908cf6f0e351e3bbc07b918e2eee5dfd4
Mahiler <fernando.mahiler@gmail.com>
2021-07-07 20:51:23 -0500

    INITIAL COMMIT

---
+++ babel.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+        alias: {
+          '@components': './src/components',
+          '@helpers': './src/helpers',
+          '@navigation': './src/navigation',
+          '@screens': './src/screens',
+          '@storage': './src/storage',
+          '@types': './src/types',
+          '@assets': './src/assets',
+          '@data': './src/data',
+          '@hooks': './src/hooks',
+        },
+      },
+    ],
+  ],
+};
